cb9887d12ba2eccd2eb216f1e850118666353917
norbit8 <norbit8@gmail.com>
2020-03-19 23:21:01 +0200

    conda

--- project_01/main.py
+++ project_01/main.py
@@ -1,5 +1,19 @@
+import numpy as np
+from plotnine import *
+from sklearn.linear_model import LinearRegression
+from pandas import DataFrame
+
 def main():
-    print("heya")
+    # print(np.arange(1,101).reshape((100, 1)))
+    X = np.arange(1,101).reshape((100,1))
+    y = np.dot(X, [7]) + 3
+    print(np.dot(X, [7]) + 3)
+    reg = LinearRegression().fit(X, y)
+    coef = np.round(reg.coef_[0])
+    print("The coefficient is "+str(coef))
+    ggplot(DataFrame({'X':X.reshape(100),'y':y.reshape(100)})) + \
+        geom_line(aes(x='X', y='y'), color="blue", size=0.8) + \
+        ggtitle("f(X) = 7X + 3")
 
 if __name__ == "__main__":
     main()
