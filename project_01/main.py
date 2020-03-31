140a22535fc8d198b14fdb3ea79f09f35a3020ce
norbit8 <norbit8@gmail.com>
2020-03-31 17:27:07 +0300

    LOL

--- project_01/main.py
+++ project_01/main.py
@@ -2,17 +2,41 @@ import numpy as np
 from plotnine import *
 from sklearn.linear_model import LinearRegression
 from pandas import DataFrame
+import matplotlib.pyplot as plt
 
 def main():
-    X = np.arange(1,101).reshape((100,1))
-    y = np.dot(X, [7]) + 3
-    reg = LinearRegression().fit(X, y)
-    coef = np.round(reg.coef_[0])
-    print("The coefficient is "+str(coef))
-    g = ggplot(DataFrame({'X':X.reshape(100),'y':y.reshape(100)})) + \
-        geom_line(aes(x='X', y='y'), color="blue", size=0.8) + \
-        ggtitle("f(X) = 7X + 3")
-    print(g)
+    # X = np.arange(1,101).reshape((100,1))
+    # y = np.dot(X, [7]) + 3
+    # reg = LinearRegression().fit(X, y)
+    # coef = np.round(reg.coef_[0])
+    # print("The coefficient is "+str(coef))
+    # g = ggplot(DataFrame({'X':X.reshape(100),'y':y.reshape(100)})) + \
+    #     geom_line(aes(x='X', y='y'), color="blue", size=0.8) + \
+    #     ggtitle("f(X) = 7X + 3")
+    # print(g)
+
+
+
+    labels = ["Arithmetic", "Empty function call", "System call"]
+    linux = [0.2, 0.2, 39.8]
+    container = [0.2, 0.2, 41]
+    vm = [0.9, 4.7, 75.1]
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width, linux, width/2, label='linux')
+    rects2 = ax.bar(x - width / 3, container, width/2, label='container')
+    rects3 = ax.bar(x + width / 3, vm, width/2, label='Virtual Machine')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Nano-Seconds (n) 10^-9')
+    ax.set_title('Time measurement of various operations')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.set_yscale('log')
+    plt.legend(bbox_to_anchor=(1, 1), loc='best', ncol=1)
+    fig.show()
 
 
     # np.random.seed(0)  # seed for reproducibility
