ffc44b599055c5f527b4cf893bb6cb86161e8676
norbit8 <norbit8@gmail.com>
2020-04-10 16:12:09 +0300

    lolzi

--- project_02/linear_model.py
+++ project_02/linear_model.py
@@ -1,22 +1,58 @@
 import numpy as np
+import pandas as pd
 from typing import Tuple
 
 
 def fit_linear_regression(mat_x: np.array, res_vec: np.array) -> Tuple[np.array, np.array]:
     """
-
+    Linear Regression solver
     Parameters:
-
-    Returns:
-
+    :param mat_x: The design matrix X (np array)
+    :param res_vec: Response Vector y (np array)
+    Returns: Tuple of the coefficient vector and the singular values of X.
     """
     ones_vec = np.ones(mat_x.shape[1])  # vectors of ones
     mat_x = np.vstack((ones_vec, mat_x))  # adding ones to the matrix
     mat_x_t = mat_x.transpose()  # transposing after adding one
     return np.linalg.pinv(mat_x_t) @ res_vec, np.diag(np.linalg.svd(mat_x_t)[1])
 
+
 def predict(x: np.array, coef_v: np.array) -> np.array:
-    
+    """
+    Prediction function
+    :param x: Design matrix.
+    :param coef_v: Coefficient vector.
+    :return: The prediction of the result vector.
+    """
+    return np.dot(x.transpose(), coef_v)
+
+
+def mse(res_vec: np.array, prediction_vec: np.array) -> float:
+    """
+    Mean Square Error function.
+    :param res_vec: Response vector.
+    :param prediction_vec: Prediction vector.
+    :return: The error.
+    """
+    return (1/float(res_vec.size)) * (np.linalg.norm(prediction_vec - res_vec)**2)
+
+
+def load_data(path: str) -> np.array:
+    """
+    Loads the data into a matrix (np array).
+    :param path: The path to the csv of the data.
+    :return: Data design matrix.
+    """
+    try:
+        data = pd.read_csv(path)
+    except FileNotFoundError:
+        print("FAILED TO FIND THE DATA LOCATION!")
+        return
+    except Exception:
+        print("AN ERROR OCCURRED WHILE LOADING THE DATA!")
+        return
+
+    print(data)
 
 if __name__ == "__main__":
     pass
--- project_02/tester.py
+++ project_02/tester.py
@@ -1,6 +1,12 @@
 from linear_model import *
 
+
 def test_01():
+    """
+    Question 9 test 1.
+    Testing the Instagram data from the TA.
+    :return:
+    """
     x = np.array([[12.3, 57.5, 28.7, 4.2], [7.5, 1.12, 0.8, 14.25]])
     ytag = np.array([46, 4, 11, 75])
     w, singulars = fit_linear_regression(x, ytag)
@@ -13,7 +19,7 @@ def test_01():
 
 def test_02():
     """
-    Question 9 test.
+    Question 9 test 2.
     randomly generating matrix and vector testing the linear regression.
     """
     mat = np.random.randint(5, size=(3, 4))
@@ -24,7 +30,20 @@ def test_02():
     print("PASSED TEST 02") if np.allclose(np.dot(mat_x.transpose().tolist(), w), y) else  print("FAILED TEST 02")
 
 
+def test_03():
+    """
+    Question 11 test.
+    """
+    pred_vec = np.array([4, 5, 6])
+    res_vec = np.array([2, 4, 1])
+    # print(mse(res_vec, pred_vec))
+    print("PASSED TEST 03") if mse(res_vec, pred_vec) == 10 else print("FAILED TEST 03")
+
+
 if __name__ == "__main__":
     np.random.seed(7)
+    print(">>>>> TESTER <<<<<")
     test_01()
     test_02()
+    test_03()
+    load_data("/home/mercydude/University/semester05/Introduction to machine learning/projects/project_02/kc_house_data.csv")
